ca59529ed48c09f8f7d54223599535cd2aac2968
mdhasan76 <mdhasan8064@gmail.com>
2025-01-25 21:37:58 +0600

    export fn

---
+++ controller/controller.go
@@ -0,0 +1,15 @@
+package controller
+
+import "fmt"
+
+func MoneDorse(s string) string {
+	return "Mode dorse" + " " + s
+}
+
+func smallLetterFn() {
+	fmt.Println("this is small letter functino")
+}
+
+func UppercasingFuncion() {
+	fmt.Println("Upper casing function")
+}
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"url-shorter/controller"
 )
 
 func main() {
 	fmt.Println("Welcome to URL shorter backend. -developed by go lang")
+	a := controller.MoneDorse("hello")
+	controller.UppercasingFuncion()
+	fmt.Println(a)
 }
